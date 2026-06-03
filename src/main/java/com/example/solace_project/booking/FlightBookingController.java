a32e65e832d82d688b787a7a56063fd79fccbc7c
jabongg <jbpvns@gmail.com>
2026-06-03 10:27:43 +0530

    added logger

--- src/main/java/com/example/solace_project/booking/FlightBookingController.java
+++ src/main/java/com/example/solace_project/booking/FlightBookingController.java
@@ -3,6 +3,7 @@ package com.example.solace_project.booking;
 import java.util.Collection;
 import java.util.Map;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/bookings")
 public class FlightBookingController {
@@ -24,23 +26,28 @@ public class FlightBookingController {
 	@PostMapping
 	@ResponseStatus(HttpStatus.ACCEPTED)
 	public FlightBooking create(@RequestBody FlightBookingRequest request) {
+		log.info("<=========inside create() method=======>");
 		return bookingService.create(request);
 	}
 
 	@GetMapping
 	public Collection<FlightBooking> findAll() {
+		log.info("<=========inside findAll() method=======>");
+
 		return bookingService.findAll();
 	}
 
 	@ExceptionHandler(BookingNotFoundException.class)
 	@ResponseStatus(HttpStatus.NOT_FOUND)
 	public Map<String, String> handleNotFound(BookingNotFoundException exception) {
+		log.info("<=========inside handleNotFound() method=======>");
 		return Map.of("error", exception.getMessage());
 	}
 
 	@ExceptionHandler(FeatureDisabledException.class)
 	@ResponseStatus(HttpStatus.NOT_FOUND)
 	public Map<String, String> handleFeatureDisabled(FeatureDisabledException exception) {
+		log.info("<=========inside handleFeatureDisabled() method=======>");
 		return Map.of("error", exception.getMessage());
 	}
 }
